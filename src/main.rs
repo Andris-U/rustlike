85f3ae462026d3d99733071f7a6e5fd8b3a6a451
Andris <aundiks@gmail.com>
2021-05-31 13:09:31 +0100

    Added startup system rendering sprite from sheet.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,34 @@ struct Position {
     z:u16
 }
 
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    // TODO: Decide on project structure and extract this into sprite loader file.
+    let texture_handle = asset_server.load("../assets/sprites/16x16.png");
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(32., 32.),
+        16,
+        16,
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite {
+                color: Color::rgb(33., 33., 33.),
+                index: 2,
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+}
+
 fn main() {
     let window_descriptor = WindowDescriptor {
         title: "rustlike".to_string(),
@@ -15,7 +43,8 @@ fn main() {
     };
 
     App::build()
-    .insert_resource(window_descriptor)
-    .add_plugins(DefaultPlugins)
-    .run();
+        .insert_resource(window_descriptor)
+        .add_startup_system(setup.system())
+        .add_plugins(DefaultPlugins)
+        .run();
 }
